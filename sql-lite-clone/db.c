0e91440649e2a52aa353600e4d24456677baac8b
jesusxy <tectihuacantec@protonmail.com>
2025-01-13 08:51:03 -0600

    feat: add switch to main

--- sql-lite-clone/db.c
+++ sql-lite-clone/db.c
@@ -4,6 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef enum
+{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum
+{
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
 typedef struct
 {
     char *buffer;         // points to a dynamically allocated memory region
@@ -46,6 +57,18 @@ void close_input_buffer(InputBuffer *input_buffer)
     free(input_buffer);
 }
 
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+
 int main(int arc, char *argv[])
 {
     InputBuffer *input_buffer = new_input_buffer();
@@ -55,14 +78,30 @@ int main(int arc, char *argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        if (input_buffer->buffer[0] == ".")
         {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
+            switch (do_meta_command(input_buffer))
+            {
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                continue;
+            }
         }
-        else
+
+        Statement statement;
+
+        switch (prepare_statement(input_buffer, &statement))
         {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        case (PREPARE_SUCESS):
+            break;
+        case (PREPARE_UNRECOGNIZED_STATEMENT):
+            print("Unrecognized keyword at start of '%s'.\n", input_buffer->buffer);
+            continue;
         }
+
+        execute_statement(&statement);
+        printf("Executed.\n");
     }
 }
