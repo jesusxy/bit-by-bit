da5b9e7339dee991dfcd34ee52fcfc4b64d46f0b
jesusxy <tectihuacantec@protonmail.com>
2025-01-14 11:06:33 -0600

    fix: reorder file to fix compiler errors

--- sql-lite-clone/db.c
+++ sql-lite-clone/db.c
@@ -8,13 +8,12 @@
 #define COLUMN_EMAIL_SIZE 255
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
 
-const uint32_t ID_SIZE = size_of_attribute(Row, id);
-const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
-const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
-const uint32_t ID_OFFSET = 0;
-const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
-const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
-const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+typedef struct
+{
+    char *buffer;         // points to a dynamically allocated memory region
+    size_t buffer_length; // size of buffer
+    ssize_t input_length; // length of input stored in the buffer (bytes read)
+} InputBuffer;
 
 typedef enum
 {
@@ -40,17 +39,6 @@ typedef enum
     STATEMENT_SELECT
 } StatementType;
 
-const uint32_t PAGE_SIZE = 4096; // 4kb is same size in virtual memory systems
-#define TABLE_MAX_PAGES 100
-const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
-const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
-
-typedef struct
-{
-    uint32_t num_rows;
-    void *pages[TABLE_MAX_PAGES];
-} Table;
-
 typedef struct
 {
     uint32_t id;
@@ -63,12 +51,25 @@ typedef struct
     StatementType type;
     Row row_to_insert;
 } Statement;
+
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
+
+const uint32_t PAGE_SIZE = 4096; // 4kb is same size in virtual memory systems
+#define TABLE_MAX_PAGES 100
+const uint32_t ROWS_PER_PAGE = PAGE_SIZE / ROW_SIZE;
+const uint32_t TABLE_MAX_ROWS = ROWS_PER_PAGE * TABLE_MAX_PAGES;
+
 typedef struct
 {
-    char *buffer;         // points to a dynamically allocated memory region
-    size_t buffer_length; // size of buffer
-    ssize_t input_length; // length of input stored in the buffer (bytes read)
-} InputBuffer;
+    uint32_t num_rows;
+    void *pages[TABLE_MAX_PAGES];
+} Table;
 
 InputBuffer *new_input_buffer()
 {
