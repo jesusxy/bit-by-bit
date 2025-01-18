3275da5afd3fd005f488393d382ef3e5f66f91a6
jesusxy <tectihuacantec@protonmail.com>
2025-01-17 20:34:28 -0600

    chore: add comments

--- sql-lite-clone/db.c
+++ sql-lite-clone/db.c
@@ -148,12 +148,12 @@ void *get_page(Pager *pager, uint32_t page_num)
 
 void *row_slot(Table *table, uint32_t row_num)
 {
-    uint32_t page_num = row_num / ROWS_PER_PAGE;
+    uint32_t page_num = row_num / ROWS_PER_PAGE; // find page num
 
     void *page = get_page(table->pager, page_num);
 
-    uint32_t row_offset = row_num % ROWS_PER_PAGE;
-    uint32_t byte_offset = row_offset * ROW_SIZE;
+    uint32_t row_offset = row_num % ROWS_PER_PAGE; // finds the row position within the page
+    uint32_t byte_offset = row_offset * ROW_SIZE;  // find exact location of row in memory
     return page + byte_offset;
 }
 
