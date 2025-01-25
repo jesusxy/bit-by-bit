c256740305ff8bab67370a7e1f66b71898a9deb2
jesusxy <tectihuacantec@protonmail.com>
2025-01-25 14:06:34 -0600

    feat: add leaf node constants

--- sql-lite-clone/db.c
+++ sql-lite-clone/db.c
@@ -20,6 +20,11 @@ typedef struct
 
 typedef enum
 {
+    NODE_INTERNAL,
+    NODE_LEAF
+} NodeType;
+typedef enum
+{
     EXECUTE_SUCCESS,
     EXECUTE_TABLE_FULL
 } ExecuteResult;
@@ -88,6 +93,66 @@ typedef struct
     uint32_t row_num;
     bool end_of_table; // indicates a position one past the last element
 } Cursor;
+
+/**
+ * Common Node Header Layout
+ */
+const uint32_t NODE_TYPE_SIZE = sizeof(uint8_t);
+const uint32_t NODE_TYPE_OFFSET = 0;
+const uint32_t IS_ROOT_SIZE = sizeof(uint8_t);
+const uint32_t IS_ROOT_OFFSET = NODE_TYPE_OFFSET;
+const uint32_t PARENT_POINTER_SIZE = sizeof(uint32_t);
+const uint32_t PARENT_POINTER_OFFSET = IS_ROOT_OFFSET + IS_ROOT_SIZE;
+const uint8_t COMMON_NODE_HEADER_SIZE = NODE_TYPE_SIZE + IS_ROOT_SIZE + PARENT_POINTER_SIZE;
+
+/**
+ * Leaf Node Header Layout
+ * These nodes will store how many "cells" they have
+ * Cells are (key, value) pairs
+ */
+
+const uint32_t LEAF_NODE_NUM_CELLS_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_NUM_CELLS_OFFSET = COMMON_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_HEADER_SIZE = COMMON_NODE_HEADER_SIZE + LEAF_NODE_NUM_CELLS_SIZE;
+
+/**
+ * Leaf Node Body Layout
+ * The body of a leaf is an array of cells.
+ * Each cell is a key followed by a value (a serialized row)
+ */
+
+const uint32_t LEAF_NODE_KEY_SIZE = sizeof(uint32_t);
+const uint32_t LEAF_NODE_KEY_OFFSET = 0;
+const uint32_t LEAF_NODE_VALUE_SIZE = ROW_SIZE;
+const uint32_t LEAF_NODE_VALUE_OFFSET = LEAF_NODE_KEY_OFFSET + LEAF_NODE_KEY_SIZE;
+const uint32_t LEAF_NODE_CELL_SIZE = LEAF_NODE_KEY_SIZE + LEAF_NODE_VALUE_SIZE;
+const uint32_t LEAF_NODE_SPACE_FOR_CELLS = PAGE_SIZE - LEAF_NODE_HEADER_SIZE;
+const uint32_t LEAF_NODE_MAX_CELLS = LEAF_NODE_SPACE_FOR_CELLS / LEAF_NODE_CELL_SIZE;
+
+uint32_t *leaf_node_num_cells(void *node)
+{
+    return node + LEAF_NODE_NUM_CELLS_OFFSET;
+}
+
+void *leaf_node_cell(void *node, uint32_t cell_num)
+{
+    return node + LEAF_NODE_HEADER_SIZE + cell_num * LEAF_NODE_CELL_SIZE;
+}
+
+uint32_t *leaf_node_key(void *node, uint32_t cell_num)
+{
+    return leaf_node_cell(node, cell_num);
+}
+
+void *leaf_node_value(void *node, uint32_t cell_num)
+{
+    return leaf_node_cell(node, cell_num) + LEAF_NODE_KEY_SIZE;
+}
+
+void initialize_leaf_node(void *node)
+{
+    *leaf_node_num_cells(node) = 0;
+}
 
 Cursor *table_start(Table *table)
 {
@@ -182,7 +247,7 @@ void *get_page(Pager *pager, uint32_t page_num)
     return pager->pages[page_num];
 }
 
-void row_slot(Table *table, uint32_t row_num) {}
+void *row_slot(Table *table, uint32_t row_num) {}
 
 void *cursor_value(Cursor *cursor)
 {
