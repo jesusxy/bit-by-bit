f909640e062184e4774e311b0f35f033bb55106b
jesusxy <tectihuacantec@protonmail.com>
2025-06-25 00:46:47 -0500

    feat: add cli commands

--- ror/cmd/ror/main.go
+++ ror/cmd/ror/main.go
@@ -1,9 +1,79 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v3"
+)
 
 const version = "v0.0.1-dev"
 
 func main() {
-	fmt.Println("ror", version)
+	root := &cli.Command{
+		Name:  "ror",
+		Usage: "Rootless OCI runner",
+		Commands: []*cli.Command{
+			{
+				Name:  "create",
+				Usage: "Create an OCI container",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:    "bundle",
+						Aliases: []string{"b"},
+						Value:   ".",
+						Usage:   "Path to the OCI bundle",
+					},
+					&cli.StringFlag{
+						Name:  "pid-file",
+						Value: "",
+						Usage: "write child pid to this file",
+					},
+				},
+				Action: func(c context.Context, cmd *cli.Command) error {
+					if cmd.Args().Len() < 1 {
+						return cli.Exit("container id required", 1)
+					}
+
+					id := cmd.Args().First()
+					bundle := cmd.String("bundle")
+					pid := cmd.String("pid-file")
+
+					fmt.Printf("Creating container { Id: %s, bundle: %s, pid: %s\n", id, bundle, pid)
+					return nil
+				},
+			},
+			{
+				Name:  "start",
+				Usage: "Start a created container",
+				Action: func(c context.Context, cmd *cli.Command) error {
+					if cmd.Args().Len() < 1 {
+						return cli.Exit("container id required", 1)
+					}
+
+					fmt.Printf("Running container %s\n", cmd.Args().First())
+
+					return nil
+				},
+			},
+			{
+				Name:  "delete",
+				Usage: "Delete container state",
+				Action: func(c context.Context, cmd *cli.Command) error {
+					if cmd.Args().Len() < 1 {
+						return cli.Exit("container id required", 1)
+					}
+
+					fmt.Printf("Deleting container %s\n", cmd.Args().First())
+					return nil
+				},
+			},
+		},
+	}
+
+	if err := root.Run(context.Background(), os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
