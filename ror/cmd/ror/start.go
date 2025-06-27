64dce9349b961bb23bcf35d34563cebf45fa2ac8
jesusxy <tectihuacantec@protonmail.com>
2025-06-27 14:40:55 -0500

    feat: add linux go file

--- ror/cmd/ror/start.go
+++ ror/cmd/ror/start.go
@@ -1,14 +1,11 @@
+//go:build !linux
+
 package main
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
-	"log"
-	"os"
-	"path/filepath"
 
-	"github.com/opencontainers/runtime-spec/specs-go"
 	"github.com/urfave/cli/v3"
 )
 
@@ -31,27 +28,5 @@ func newStartCmd() *cli.Command {
 }
 
 func startContainer(id string) error {
-	// locate the container state via id
-	containerState := filepath.Join(defaultBasePath, id)
-
-	if _, err := os.Stat(containerState); os.IsNotExist(err) {
-		// If we get an "IsNotExist" error, it means the directory isn't there.
-		return fmt.Errorf("container with id '%s' does not exist", id)
-	}
-
-	// Load the blueprint (config.json)
-	configJSON, err := os.ReadFile(filepath.Join(containerState, "config.json"))
-	if err != nil {
-		return fmt.Errorf("failed to read bundle config: %w", err)
-	}
-
-	// unmarshall config into spec struct
-	var spec specs.Spec
-	if err := json.Unmarshal(configJSON, &spec); err != nil {
-		return fmt.Errorf("failed to unmarshall bundle into OCI spec: %w", err)
-	}
-
-	log.Printf("Successfully loaded spec for container '%s'. Starting...", id)
-
-	return nil
+	return fmt.Errorf("cannot start container on non-linux OS")
 }
