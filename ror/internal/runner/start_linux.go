759c536d0d4dbd76805170756530f385ebb8d563
jesusxy <tectihuacantec@protonmail.com>
2025-07-26 17:49:25 +0000

    feat: update startcontainer

--- ror/internal/runner/start_linux.go
+++ ror/internal/runner/start_linux.go
@@ -34,16 +34,14 @@ func (r *Runner) StartContainer(id string) error {
 		return fmt.Errorf("failed to unmarshal container spec: %w", err)
 	}
 	// --- prepare the command to run ---
-	executable, err := os.Executable()
-	if err != nil {
-		return fmt.Errorf("could not find executable path: %w", err)
-	}
-	cmd := exec.Command(executable, "init", id)
+	cmd := exec.Command("/proc/self/exe", "init", id)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 
 	var cloneFlags uintptr
+	hasUserNS := false
+
 	for _, ns := range spec.Linux.Namespaces {
 		switch ns.Type {
 		case "user":
@@ -61,50 +59,27 @@ func (r *Runner) StartContainer(id string) error {
 		}
 	}
 
-	if cloneFlags&syscall.CLONE_NEWUSER == 0 {
+	if !hasUserNS {
 		return fmt.Errorf("user namespace is required for rootless containers")
 	}
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags:                 cloneFlags,
-		UidMappings:                []syscall.SysProcIDMap{},
-		GidMappings:                []syscall.SysProcIDMap{},
 		GidMappingsEnableSetgroups: false,
 	}
-
-	for _, mapping := range spec.Linux.UIDMappings {
-		cmd.SysProcAttr.UidMappings = append(cmd.SysProcAttr.UidMappings, syscall.SysProcIDMap{
-			ContainerID: int(mapping.ContainerID),
-			HostID:      int(mapping.HostID),
-			Size:        int(mapping.Size),
-		})
-	}
-
-	for _, mapping := range spec.Linux.GIDMappings {
-		cmd.SysProcAttr.GidMappings = append(cmd.SysProcAttr.GidMappings, syscall.SysProcIDMap{
-			ContainerID: int(mapping.ContainerID),
-			HostID:      int(mapping.HostID),
-			Size:        int(mapping.Size),
-		})
-	}
-
-	if len(cmd.SysProcAttr.UidMappings) == 0 {
-		return fmt.Errorf("at leas one UID mapping is required for rootless containers")
-	}
-
-	if len(cmd.SysProcAttr.GidMappings) == 0 {
-		return fmt.Errorf("at least one GID mapping is required for rootless containers")
-	}
-
-	log.Printf("Starting container with namespaces: clone_flags=0x%x", cloneFlags)
-	log.Printf("UID mappings: %+v", cmd.SysProcAttr.UidMappings)
-	log.Printf("GID mappings: %+v", cmd.SysProcAttr.GidMappings)
 
 	if err := cmd.Start(); err != nil {
 		return fmt.Errorf("failed to start container init process: %w", err)
 	}
 
 	pid := cmd.Process.Pid
+	log.Printf("Container process started with PID: %d", pid)
+
+	if err := writeIDMappings(pid, spec); err != nil {
+		cmd.Process.Kill()
+		return fmt.Errorf("failed to write ID mappings: %w", err)
+	}
+
 	pidFilePath := filepath.Join(containerStatePath, "pid")
 
 	if err := os.WriteFile(pidFilePath, []byte(fmt.Sprintf("%d", pid)), 0644); err != nil {
@@ -112,4 +87,37 @@ func (r *Runner) StartContainer(id string) error {
 	}
 
 	return cmd.Wait()
+}
+
+func writeIDMappings(pid int, spec specs.Spec) error {
+	uidMapPath := fmt.Sprintf("/proc/%d/uid_map", pid)
+	uidMapContent := ""
+
+	for _, mapping := range spec.Linux.UIDMappings {
+		uidMapContent += fmt.Sprintf("%d %d %d\n", mapping.ContainerID, mapping.HostID, mapping.Size)
+	}
+
+	log.Printf("Writing UID mappings to %s: %s", uidMapPath, uidMapContent)
+	if err := os.WriteFile(uidMapPath, []byte(uidMapContent), 0644); err != nil {
+		return fmt.Errorf("failed to write uid_map: %w", err)
+	}
+
+	setGroupsPath := fmt.Sprintf("/proc/%d/setgroups", pid)
+	if err := os.WriteFile(setGroupsPath, []byte("deny"), 0644); err != nil {
+		return fmt.Errorf("failed to write setgroups: %w", err)
+	}
+
+	gidMapPath := fmt.Sprintf("/proc/%d/gid_map", pid)
+	gidMapContent := ""
+
+	for _, mapping := range spec.Linux.GIDMappings {
+		gidMapContent += fmt.Sprintf("%d %d %d\n", mapping.ContainerID, mapping.HostID, mapping.Size)
+	}
+
+	log.Printf("Writing GID mappings to %s: %s", gidMapPath, gidMapContent)
+	if err := os.WriteFile(gidMapPath, []byte(gidMapContent), 0644); err != nil {
+		return fmt.Errorf("failed to write gid_map: %w", err)
+	}
+
+	return nil
 }
