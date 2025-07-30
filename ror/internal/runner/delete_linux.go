2a5133558945dcfca5a14eba536c58aef7619a46
jesusxy <tectihuacantec@protonmail.com>
2025-07-30 02:39:42 +0000

    feat: update delete container logic

--- ror/internal/runner/delete_linux.go
+++ ror/internal/runner/delete_linux.go
@@ -3,7 +3,9 @@
 package runner
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -13,11 +15,33 @@ import (
 // would I pass the processId here as an arg?
 func (r *Runner) DeleteContainer(id string) error {
 	containerStatePath := filepath.Join(r.BasePath, id)
+
+	if _, err := os.Stat(containerStatePath); err != nil {
+		return fmt.Errorf("container %s does not exist", id)
+	}
+
+	if err := r.terminateContainerProcess(containerStatePath); err != nil {
+		log.Printf("Failed to terminate process for container %s: %v", id, err)
+	}
+
+	if err := os.RemoveAll(containerStatePath); err != nil {
+		return fmt.Errorf("failed to remove container directory: %w", err)
+	}
+	fmt.Printf("Container %s deleted\n", id)
+
+	return nil
+}
+
+func (r *Runner) terminateContainerProcess(containerStatePath string) error {
 	pidFilePath := filepath.Join(containerStatePath, "pid")
 
 	// i think im missing the pid in the path here when reading from the containers state dir
 	content, err := os.ReadFile(pidFilePath)
 	if err != nil {
+		if os.IsNotExist(err) {
+			return nil
+		}
+
 		return fmt.Errorf("failed to read contents from pid file: %w", err)
 	}
 
@@ -27,8 +51,14 @@ func (r *Runner) DeleteContainer(id string) error {
 	}
 
 	if err := syscall.Kill(pid, syscall.SIGKILL); err != nil {
-		fmt.Printf("failed to kill process %d: %v\n", pid, err)
+		if errors.Is(err, syscall.ESRCH) {
+			return nil // process doesnt exist
+		}
+		if err := syscall.Kill(pid, syscall.SIGKILL); err != nil && !errors.Is(err, syscall.ESRCH) {
+			return fmt.Errorf("failed to kill process %d: %w", pid, err)
+		}
 	}
 
-	return os.RemoveAll(containerStatePath)
+	log.Printf("Terminated process %d", pid)
+	return nil
 }
