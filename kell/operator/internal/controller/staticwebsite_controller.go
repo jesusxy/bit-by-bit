6429a4ab9e65a1f2d4fdb16c6d019842e21d59cd
isa <53798994+jesusxy@users.noreply.github.com>
2025-08-04 14:57:42 -0500

    feat: Enable proper GitOps workflow by fixing reconciliation logic (#4)
    * feat: update reconciliation fn
    
    * feat: add helpers and refactor udpate logic

--- kell/operator/internal/controller/staticwebsite_controller.go
+++ kell/operator/internal/controller/staticwebsite_controller.go
@@ -18,6 +18,7 @@ package controller
 
 import (
 	"context"
+	"reflect"
 
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
@@ -30,6 +31,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
 	webappv1alpha1 "github.com/jesusxy/bit-by-bit/kell/operator/api/v1alpha1"
 )
 
@@ -71,65 +73,141 @@ func (r *StaticWebsiteReconciler) Reconcile(ctx context.Context, req ctrl.Reques
 		return ctrl.Result{}, err
 	}
 
+	// --- Deployment Reconciliation ---
 	foundDeployment := &appsv1.Deployment{}
 	err = r.Get(ctx, types.NamespacedName{Name: staticWebsite.Name, Namespace: staticWebsite.Namespace}, foundDeployment)
 
 	if err != nil && errors.IsNotFound(err) {
-		// define new deployment
-		dep := r.deploymentForStaticWebsite(staticWebsite)
-		logger.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
-		err = r.Create(ctx, dep)
-
+		// create new dployment
+		desiredDeployment := r.deploymentForStaticWebsite(staticWebsite)
+		logger.Info("Creating New Deployment", "Deployment.Namespace", desiredDeployment.Namespace, "Deployment.Name", desiredDeployment.Name)
+		err = r.Create(ctx, desiredDeployment)
 		if err != nil {
-			logger.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			logger.Error(err, "Failed to create new Deployment")
 			return ctrl.Result{}, err
 		}
-
-		// deployment created successfully - return and requeue
-		return ctrl.Result{Requeue: true}, nil
+		return ctrl.Result{}, nil
 	} else if err != nil {
 		logger.Error(err, "Failed to get deployment")
 		return ctrl.Result{}, err
 	}
 
-	// ensure deployment size is the same as spec
-	size := staticWebsite.Spec.Replicas
-	if *foundDeployment.Spec.Replicas != size {
-		foundDeployment.Spec.Replicas = &size
+	desiredDeployment := r.deploymentForStaticWebsite(staticWebsite)
+	if r.deploymentNeedsUpdate(foundDeployment, desiredDeployment, logger) {
+		logger.Info("Deployment spec is out of date, updating...")
+
+		r.updateDeploymentSpec(foundDeployment, desiredDeployment)
+
 		err = r.Update(ctx, foundDeployment)
 		if err != nil {
-			logger.Error(err, "Failed to update Deployment", "Deployment.Namespace", foundDeployment.Namespace, "Deployment.Name", foundDeployment.Name)
-			return ctrl.Result{}, nil
+			logger.Error(err, "Failed to update deployment")
+			return ctrl.Result{}, err
 		}
 
-		return ctrl.Result{Requeue: true}, nil
+		return ctrl.Result{}, nil
 	}
 
+	// --- Service Reconciliation ---\
 	foundService := &corev1.Service{}
 	err = r.Get(ctx, types.NamespacedName{Name: staticWebsite.Name, Namespace: staticWebsite.Namespace}, foundService)
 
 	if err != nil && errors.IsNotFound(err) {
 		// define a new service
-		svc := r.serviceForStaticWebsite(staticWebsite)
-		logger.Info("Creating a new Service", "Service.Namespace", svc.Namespace, "Service.Name", svc.Name)
-		err = r.Create(ctx, svc)
+		desiredService := r.serviceForStaticWebsite(staticWebsite)
+		logger.Info("Creating a new Service", "Service.Namespace", desiredService.Namespace, "Service.Name", desiredService.Name)
+		err = r.Create(ctx, desiredService)
 		if err != nil {
-			logger.Error(err, "Failed to create a new Service", "Service.Namespace", svc.Namespace, "Service.Name", svc.Name)
+			logger.Error(err, "Failed to create a new Service", "Service.Namespace", desiredService.Namespace, "Service.Name", desiredService.Name)
 			return ctrl.Result{}, err
 		}
-		return ctrl.Result{Requeue: true}, nil
+		return ctrl.Result{}, nil
 	} else if err != nil {
 		logger.Error(err, "Failed to get Service")
 		return ctrl.Result{}, err
 	}
 
+	// a real world operator would also compare the desiredService.Spec with the foundService.Spec and update if needed
+
 	// all resources are in the desired state
 	return ctrl.Result{}, nil
 }
 
+func (r *StaticWebsiteReconciler) deploymentNeedsUpdate(found, desired *appsv1.Deployment, logger logr.Logger) bool {
+	if found.Spec.Replicas == nil || *found.Spec.Replicas != *desired.Spec.Replicas {
+		logger.Info("Replica count differs", "found", found.Spec.Replicas, "desired", *desired.Spec.Replicas)
+		return true
+	}
+
+	foundInitContainers := r.normalizeContainers(found.Spec.Template.Spec.InitContainers)
+	desiredInitContainers := r.normalizeContainers(desired.Spec.Template.Spec.InitContainers)
+
+	if !reflect.DeepEqual(foundInitContainers, desiredInitContainers) {
+		logger.Info("Init containers differ")
+		logger.Info("Found init containers", "containers", foundInitContainers)
+		logger.Info("Desired init containers", "containers", desiredInitContainers)
+		return true
+	}
+
+	foundContainers := r.normalizeContainers(found.Spec.Template.Spec.Containers)
+	desiredContainers := r.normalizeContainers(desired.Spec.Template.Spec.Containers)
+
+	if !reflect.DeepEqual(foundContainers, desiredContainers) {
+		logger.Info("Main containers differ")
+		logger.Info("Found containers", "containers", foundContainers)
+		logger.Info("Desired containers", "containers", desiredContainers)
+		return true
+	}
+
+	if !reflect.DeepEqual(found.Spec.Template.Spec.Volumes, desired.Spec.Template.Spec.Volumes) {
+		logger.Info("Volumes differ")
+		logger.Info("Found volumes", "volumes", found.Spec.Template.Spec.Volumes)
+		logger.Info("Desired volumes", "volumes", desired.Spec.Template.Spec.Volumes)
+		return true
+	}
+
+	return false
+}
+
+func (r *StaticWebsiteReconciler) normalizeContainers(containers []corev1.Container) []corev1.Container {
+	normalized := make([]corev1.Container, len(containers))
+	copy(normalized, containers)
+
+	for i := range normalized {
+		// clear fields that Kubernetes auto-populates
+		normalized[i].TerminationMessagePath = ""
+		normalized[i].TerminationMessagePolicy = ""
+		normalized[i].ImagePullPolicy = ""
+
+		if normalized[i].Resources.Limits == nil && normalized[i].Resources.Requests == nil {
+			normalized[i].Resources = corev1.ResourceRequirements{}
+		}
+
+		if normalized[i].SecurityContext != nil {
+			if normalized[i].SecurityContext.RunAsNonRoot == nil &&
+				normalized[i].SecurityContext.ReadOnlyRootFilesystem == nil &&
+				normalized[i].SecurityContext.AllowPrivilegeEscalation == nil &&
+				normalized[i].SecurityContext.RunAsUser == nil &&
+				normalized[i].SecurityContext.RunAsGroup == nil {
+				normalized[i].SecurityContext = nil
+			}
+		}
+	}
+
+	return normalized
+}
+
+func (r *StaticWebsiteReconciler) updateDeploymentSpec(found, desired *appsv1.Deployment) {
+	found.Spec.Replicas = desired.Spec.Replicas
+	found.Spec.Template.Spec.InitContainers = desired.Spec.Template.Spec.InitContainers
+	found.Spec.Template.Spec.Containers = desired.Spec.Template.Spec.Containers
+	found.Spec.Template.Spec.Volumes = desired.Spec.Template.Spec.Volumes
+
+	if !reflect.DeepEqual(found.Spec.Template.ObjectMeta.Labels, desired.Spec.Template.ObjectMeta.Labels) {
+		found.Spec.Template.ObjectMeta.Labels = desired.Spec.Template.ObjectMeta.Labels
+	}
+}
+
 func (r *StaticWebsiteReconciler) deploymentForStaticWebsite(sw *webappv1alpha1.StaticWebsite) *appsv1.Deployment {
-	logger := log.FromContext(context.Background())
-	logger.Info(">>> Using V3 deployment logic with bitnami/git and explicit command <<<")
 	labels := map[string]string{"app": sw.Name}
 	replicas := sw.Spec.Replicas
 
@@ -182,6 +260,7 @@ func (r *StaticWebsiteReconciler) deploymentForStaticWebsite(sw *webappv1alpha1.
 						Ports: []corev1.ContainerPort{{
 							ContainerPort: 80,
 							Name:          "http",
+							Protocol:      corev1.ProtocolTCP,
 						}},
 						VolumeMounts: []corev1.VolumeMount{{
 							Name:      "web-content",
