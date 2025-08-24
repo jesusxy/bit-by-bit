52988be2af3aa7ef7282696a10a9f2ab0e3b7214
jesusxy <tectihuacantec@protonmail.com>
2025-08-24 01:15:48 -0500

    feat: add log parsers registry

--- nox/internal/ingester/ingester.go
+++ nox/internal/ingester/ingester.go
@@ -6,10 +6,41 @@ import (
 	"log"
 	"nox/internal/model"
 	"os"
+	"regexp"
+	"time"
 )
 
-func ParseLog(logln string) (model.Event, error) {
+type logParser struct {
+	EventType string
+	Regex     *regexp.Regexp
+	Builder   func(matches []string) (model.Event, error)
+}
 
+var parsers = []logParser{
+	{
+		EventType: "SSHD_Failed_Password",
+		Regex:     regexp.MustCompile(`Failed password for .*?(\S+) from ([\d\.]+) port \d+ ssh2`),
+		Builder: func(matches []string) (model.Event, error) {
+			return model.Event{
+				Timestamp: time.Now().UTC(),
+				EventType: "SSHD_Failed_Password",
+				Source:    matches[2],
+				Metadata:  map[string]string{"user": matches[1]},
+			}, nil
+		},
+	},
+}
+
+func ParseLog(logln string) (model.Event, error) {
+	for _, parser := range parsers {
+		matches := parser.Regex.FindStringSubmatch(logln)
+
+		if len(matches) > 0 {
+			return parser.Builder(matches)
+		}
+	}
+
+	return model.Event{}, model.ErrIgnoredLine
 }
 
 func ReadFile(fpath string) ([]model.Event, error) {
