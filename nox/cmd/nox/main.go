32078f332b3044d2a8937b2bf40b61a45faf202a
jesusxy <tectihuacantec@protonmail.com>
2025-08-24 01:45:09 -0500

    feat: wire up the engine logic

--- nox/cmd/nox/main.go
+++ nox/cmd/nox/main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"nox/internal/ingester"
+	"nox/internal/rules"
+)
+
+func main() {
+	stateManager := rules.NewStateManager()
+
+	events, err := ingester.ReadFile("path/to/your_log") // how do i create fake logs or is there an api i can use?
+	if err != nil {
+		log.Printf("failed to read file %w\n", err)
+	}
+
+	for _, event := range events {
+		triggeredAlerts := rules.EvaluateEvent(event, stateManager)
+
+		for _, alert := range triggeredAlerts {
+			fmt.Printf("ALERT: [%s] %s\n", alert.RuleName, alert.Message)
+		}
+	}
+}
