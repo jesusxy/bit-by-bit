aeee1dc3da35a464430f7363b107562c680ce6d4
jesusxy <tectihuacantec@protonmail.com>
2025-08-25 14:30:02 -0500

    feat: add extract fn update regex method

--- nox/internal/ingester/ingester.go
+++ nox/internal/ingester/ingester.go
@@ -5,6 +5,8 @@ import (
 	"log/slog"
 	"nox/internal/model"
 	"regexp"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/hpcloud/tail"
@@ -14,17 +16,21 @@ import (
 // Aug 24 13:30:00 my-server sshd[8888]: Accepted password for jsmith from 192.168.1.50 port 12345 ssh2
 // type=EXECVE msg=audit(1692889800.123:1): argc=3 a0="ls" a1="-la" a2="/tmp" pid=1234 ppid=567 auid=0 uid=0 gid=0
 
+type SimpleBuilder func(matches []string) (model.Event, error)
+type AuditBuilder func(matches []string, auditLine string) (model.Event, error)
+
 type logParser struct {
-	EventType string
-	Regex     *regexp.Regexp
-	Builder   func(matches []string) (model.Event, error)
+	EventType     string
+	Regex         *regexp.Regexp
+	SimpleBuilder SimpleBuilder
+	AuditBuilder  AuditBuilder
 }
 
 var parsers = []logParser{
 	{
 		EventType: "SSHD_Failed_Password",
 		Regex:     regexp.MustCompile(`Failed password for .*?(\S+) from ([\d\.]+) port \d+ ssh2`),
-		Builder: func(matches []string) (model.Event, error) {
+		SimpleBuilder: func(matches []string) (model.Event, error) {
 			return model.Event{
 				Timestamp: time.Now().UTC(),
 				EventType: "SSHD_Failed_Password",
@@ -36,7 +42,7 @@ var parsers = []logParser{
 	{
 		EventType: "SSHD_Accepted_Password",
 		Regex:     regexp.MustCompile(`Accepted password for (\S+) from ([\d\.]+) port \d+ ssh2`),
-		Builder: func(matches []string) (model.Event, error) {
+		SimpleBuilder: func(matches []string) (model.Event, error) {
 			return model.Event{
 				Timestamp: time.Now().UTC(),
 				EventType: "SSHD_Accepted_Password",
@@ -47,20 +53,71 @@ var parsers = []logParser{
 	},
 	{
 		EventType: "Process_Executed",
-		Regex:     regexp.MustCompile(`\d{2}:\d{2}:\d{2}\s+(\S+)\s+(\d+)\s+.*\s+0\s+(.*)`),
-		Builder: func(matches []string) (model.Event, error) {
+		Regex:     regexp.MustCompile(`type=EXECVE msg=audit\(([0-9.]+):\d+\):(.*?)pid=(\d+) ppid=(\d+).*?uid=(\d+) gid=(\d+)`),
+		AuditBuilder: func(matches []string, auditLine string) (model.Event, error) {
+			timestampFloat, err := strconv.ParseFloat(matches[1], 64)
+			if err != nil {
+				timestampFloat = float64(time.Now().Unix())
+			}
+			timestamp := time.Unix(int64(timestampFloat), int64((timestampFloat-float64(int64(timestampFloat)))*1e9))
+
+			command, args := extractCommandFromAudit(auditLine)
+			fullCommand := command
+
+			if len(args) > 0 {
+				fullCommand = command + " " + strings.Join(args, " ")
+			}
+
 			return model.Event{
-				Timestamp: time.Now().UTC(),
+				Timestamp: timestamp,
 				EventType: "Process_Executed",
 				Source:    "localhost",
 				Metadata: map[string]string{
-					"process_name": matches[1],
-					"pid":          matches[2],
-					"command":      matches[3],
+					"process_name": command,
+					"pid":          matches[3],
+					"ppid":         matches[4],
+					"uid":          matches[5],
+					"gid":          matches[6],
+					"command":      fullCommand,
+					"args":         strings.Join(args, " "),
 				},
 			}, nil
 		},
 	},
+}
+
+func extractCommandFromAudit(auditLine string) (command string, args []string) {
+	argcRegex := regexp.MustCompile(`argc=(\d+)`)
+	argcMatches := argcRegex.FindStringSubmatch(auditLine)
+
+	if len(argcMatches) < 2 {
+		return "", nil
+	}
+
+	argc, err := strconv.Atoi(string(argcMatches[1]))
+	if err != nil {
+		return "", nil
+	}
+
+	var allArgs []string
+	for i := 0; i < argc; i++ {
+		argRegex := regexp.MustCompile(`a` + strconv.Itoa(i) + `="([^"]*)`)
+		argMatches := argRegex.FindStringSubmatch(auditLine)
+		if len(argMatches) >= 2 {
+			allArgs = append(allArgs, argMatches[1])
+		}
+	}
+
+	if len(allArgs) == 0 {
+		return "", nil
+	}
+
+	command = allArgs[0]
+	if len(allArgs) > 1 {
+		args = allArgs[1:]
+	}
+
+	return command, args
 }
 
 func ParseLog(logln string) (model.Event, error) {
@@ -68,7 +125,11 @@ func ParseLog(logln string) (model.Event, error) {
 		matches := parser.Regex.FindStringSubmatch(logln)
 
 		if len(matches) > 0 {
-			return parser.Builder(matches)
+			if parser.SimpleBuilder != nil {
+				return parser.SimpleBuilder(matches)
+			} else if parser.AuditBuilder != nil {
+				return parser.AuditBuilder(matches, logln)
+			}
 		}
 	}
 
