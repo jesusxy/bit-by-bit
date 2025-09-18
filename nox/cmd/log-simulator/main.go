275ddb50532b5385377bd6452f75c4977de702f6
jesusxy <tectihuacantec@protonmail.com>
2025-09-18 10:34:51 -0500

    fix: update log simulator

--- nox/cmd/log-simulator/main.go
+++ nox/cmd/log-simulator/main.go
@@ -10,12 +10,15 @@ import (
 )
 
 const (
-	logFile               = "testdata/auth.log"
+	logFile             = "testdata/auth.log"
+	sshdTimeFormat      = "Jan _2 15:04:05"
+	execsnoopTimeFormat = time.RFC3339
+)
+
+var (
 	failedLoginTemplate   = "%s my-server sshd[%d]: Failed password for %s from %s port %d ssh2\n"
 	acceptedLoginTemplate = "%s my-server sshd[%d]: Accepted password for %s from %s port %d ssh2\n"
 	execsnoopTemplate     = "%s %d %s %d %d %d %s\n"
-	sshdTimeFormat        = "Jan _2 15:04:05"
-	execsnoopTimeFormat   = time.RFC3339
 )
 
 type Scenario struct {
@@ -41,9 +44,9 @@ func main() {
 	flag.Parse()
 
 	if *testcase != "" {
-		runTargetedTest(*testcase)
+		runScenario(*testcase)
 	} else {
-		runChaosSimulation()
+		runContinousSimulation()
 	}
 }
 
@@ -60,13 +63,7 @@ func appendLog(message string) {
 	}
 }
 
-func appendAndPrint(message, logLine string) {
-	fmt.Print(message)
-	appendLog(logLine)
-	fmt.Println(" -> Done")
-}
-
-func runTargetedTest(name string) {
+func runScenario(name string) {
 	log.Printf("Starting targeted log simulation for '%s'...", name)
 	switch name {
 	case "bruteforce":
@@ -156,60 +153,48 @@ func runTargetedTest(name string) {
 	log.Println("Simulation finished.")
 }
 
-func runChaosSimulation() {
-	log.Println("Starting random 'chaos' attack log simulation...")
+func runContinousSimulation() {
+	log.Println("Starting random 'chaos' attack log simulation... (Press Ctrl+C to stop)")
 
 	for {
-		sleepDuration := time.Duration(rand.Intn(4)+1) * time.Second
-		time.Sleep(sleepDuration)
 
-		scenario := rand.Intn(5)
-		var logLine, logMessage string
+		f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Printf("Error opening log file: %v", err)
+			continue
+		}
 
-		switch scenario {
+		var logLine string
+		event_type := rand.Intn(3)
+
+		switch event_type {
 		case 0:
 			// Scenario: single suspicious command
 			scenario := suspiciousScenarios[rand.Intn(len(suspiciousScenarios))]
-			logMessage = fmt.Sprintf("Injecting: Suspicious command (%s)", scenario.Name)
+			log.Printf("Injecting: Suspicious command (%s)", scenario.Name)
 			timestamp := time.Now().Format("15:04:05")
 			pid, ppid := rand.Intn(9000)+1000, rand.Intn(90000)+1000
 			logLine = fmt.Sprintf(execsnoopTemplate, timestamp, scenario.UID, scenario.Command, pid, ppid, 0, scenario.FullCommand)
-			appendAndPrint(logMessage, logLine)
 		case 1:
-			// Scenario: rapid execution
-			logMessage = "Injecting: Rapid Process Execution Burst"
-			fmt.Print(logMessage)
-			for i := 0; i < 15; i++ {
-				timestamp := time.Now().Format("15:04:05")
-				pid, ppid := rand.Intn(90000)+1000, rand.Intn(90000)+1000
-				logLine = fmt.Sprintf(execsnoopTemplate, timestamp, 1000, "ls", pid, ppid, 0, "/bin/ls")
-				appendAndPrint(logMessage, logLine)
-				time.Sleep(100 * time.Millisecond)
-			}
+			log.Printf("Injecting: Successful Login for jsmith")
+			timestamp := time.Now().Format("Jan  2 15:04:05")
+			pid, port := rand.Intn(9000)+1000, rand.Intn(60000)+1024
+			logLine = fmt.Sprintf(acceptedLoginTemplate, timestamp, pid, "jsmith", "193.99.144.80", port)
 		case 2:
-			logMessage = "Injecting: Successful Login for jsmith"
-			timestamp := time.Now().Format("Jan  2 15:04:05")
-			pid, port := rand.Intn(9000)+1000, rand.Intn(60000)+1024
-			logLine := fmt.Sprintf(acceptedLoginTemplate, timestamp, pid, "jsmith", "193.99.144.80", port)
-			appendAndPrint(logMessage, logLine)
-		case 3:
-			// Scenario: Brute force ssh attack (for checkFailedLogins)
-			attackIP := fmt.Sprintf("198.51.100.%d", rand.Intn(254)+1)
-			logMessage = fmt.Sprintf("Injecting: SSH Brute-Force from %s", attackIP)
-			for i := 0; i < 6; i++ {
-				timestamp := time.Now().Format("Jan  2 15:04:05")
-				pid, port := rand.Intn(9000)+1000, rand.Intn(60000)+1024
-				logLine := fmt.Sprintf(failedLoginTemplate, timestamp, pid, "root", attackIP, port)
-				appendAndPrint(logMessage, logLine)
-			}
-		case 4:
 			// Single random failed Login
-			logMessage = "Injecting: Random failed login"
-			timestamp := time.Now().Format("Jan  2 15:04:05")
-			pid, port := rand.Intn(9000)+1000, rand.Intn(60000)+1024
 			ip := fmt.Sprintf("10.10.10.%d", rand.Intn(254)+1)
-			logLine := fmt.Sprintf(failedLoginTemplate, timestamp, pid, "admin", ip, port)
-			appendAndPrint(logMessage, logLine)
+			log.Printf("Injecting: Random failed login from %s", ip)
+			timestamp := time.Now().Format(sshdTimeFormat)
+			pid, port := rand.Intn(9000)+1000, rand.Intn(60000)+1024
+			logLine = fmt.Sprintf(failedLoginTemplate, timestamp, pid, "admin", ip, port)
 		}
+
+		if _, err := f.WriteString(logLine); err != nil {
+			log.Printf("failed to write to log file: %v", err)
+		}
+
+		f.Close()
+
+		time.Sleep(time.Duration(rand.Intn(4)+1) * time.Second)
 	}
 }
